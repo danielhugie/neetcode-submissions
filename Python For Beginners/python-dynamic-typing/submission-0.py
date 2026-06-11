15e99e030a2c8cbfe025fa58df4cbeddbdff94a3
Daniel Hugie <43250298+danielhugie@users.noreply.github.com>
2026-06-11 11:41:43 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = [1, 3, 5]
+print(type(variable))
